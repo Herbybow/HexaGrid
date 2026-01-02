7ab18d1f6b573696b6bcd34aec98b66832b144cc
Herbybow <judikael.jo@hotmail.fr>
2026-01-02 19:40:42 +0100

    index.js

--- _EXPORT_/server/index.js
+++ _EXPORT_/server/index.js
@@ -93,7 +93,7 @@ app.post('/api/background', upload.single('background'), (req, res) => {
 });
 
 // Any request that doesn't match an API route or static file gets sent to index.html
-app.get('*', (req, res, next) => {
+app.get('(.*)', (req, res, next) => {
     if (req.path.startsWith('/api') || req.path.startsWith('/uploads')) return next();
     res.sendFile(path.join(clientDistPath, 'index.html'));
 });
@@ -435,3 +435,4 @@ const PORT = process.env.PORT || 3001;
 httpServer.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
